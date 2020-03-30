f38e32dec5a2652e03f8a284f2a236c58f8d8638
Colton Kaiser <kaiserc2@seattleu.edu>
2020-03-30 02:53:30 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,15 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  if array.include?(value_to_find)
+    p array.find_index(value_to_find)
+  end
 end
 
 def find_max_value(array)
-  # Add your solution here
+  array.sort!
+  p array[array.length - 1]
 end
 
 def find_min_value(array)
-  # Add your solution here
+  array.sort!
+  p array[0]
 end
